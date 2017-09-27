13084e918d51e76b1d4cc6451e4a322ce8aeb736
cpiehl <charlespiehl@gmail.com>
2017-09-27 11:32:06 -0700

    upload main_example.py

---
+++ main_example.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import Karel
+
+def turnright():
+	karel.turnleft()
+	karel.turnleft()
+	karel.turnleft()
+
+karel = Karel.Robot(1, 0, Karel.SOUTH, beepers=100)
+
+world = Karel.World("worlds/10x20-room.txt")
+world.add_robot(karel)
+world.set_speed(0.1)
+
+karel.move()
+karel.move()
+karel.turnleft()
+karel.move()
+karel.move()
+karel.move()
+turnright()
+karel.move()
+karel.move()
